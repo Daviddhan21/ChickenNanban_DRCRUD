0cb62fc1c9b11db51b87e0f9ac1f78b503af0d3d
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-04-09 18:07:40 +0700

    mengatur tampilan awal form dan komponen UI

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -15,24 +15,20 @@ namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
-        
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+        "Data Source=LAPTOP-DAP28EO2\\RAYVANZA_DAVID;Initial Catalog=DBAkademikADO;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
 
         // ───────────────────────────────────────────
         // FORM LOAD
         // ───────────────────────────────────────────
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            cmbJK.Items.Clear();
-            cmbJK.Items.Add("L");
-            cmbJK.Items.Add("P");
-
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView1.MultiSelect = false;
-            dataGridView1.ReadOnly = true;
-            dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-
-            dataGridView1.CellClick += new DataGridViewCellEventHandler(dataGridView1_CellClick);
+        
         }
 
         // ───────────────────────────────────────────
